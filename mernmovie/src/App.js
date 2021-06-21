25f38dae7f6a5e9bac48e561363c02ef4b3385ab
markh1089 <75758620+markh1089@users.noreply.github.com>
2021-06-21 21:17:32 +0100

    Added table with movies and filter for deletion

--- mernmovie/src/App.js
+++ mernmovie/src/App.js
@@ -4,7 +4,6 @@ import Movies from './Components/movies';
 function App() {
   return (
     <main className="container">
-      <h1> Hello World </h1>
       <Movies/>
 
     </main>
--- mernmovie/src/Components/movies.jsx
+++ mernmovie/src/Components/movies.jsx
@@ -1,15 +1,49 @@
 import React, { Component } from 'react'
-import {getMovies} from "..fakeMovieService"
+import {getMovies} from "../services/fakeMovieService"
 
  class Movies extends Component {
      state = {
-         movie: getMovies()
+         movies: getMovies()
      };
 
+     handleDelete = (movie) => {
+         const movies = this.state.movies.filter(m => m._id !== movie._id)
+         this.setState({movies: movies})
+
+     }
+
     render() {
+        if (this.state.movies.length === 0) return <p>There are no movies in the database</p>;
+        
         return (
             <div>
-                <h3>Showing {console.log(this.state.movie)} movies in the database</h3>
+                <h3>Showing {this.state.movies.length} movies in the database</h3>
+
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Genre</th>
+                            <th>Stock</th>
+                            <th>Rate</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.movies.map(movie => (
+                        <tr key={movie._id}>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                            <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+
+                        </tr>
+
+                        ))}
+                    </tbody>
+                </table>
+
                 
             </div>
         )
